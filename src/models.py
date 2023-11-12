9691bfe0dc781d63dc62c2cada13ac84a443e19b
BaptPicxDev <picard.baptiste@laposte.net>
2023-11-12 11:25:02 +0000

    Checkconstraint

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import CheckConstraint
 from sqlalchemy.ext.declarative import declarative_base
+
 
 from src.utils import generate_uuid
 
@@ -15,7 +16,7 @@ class Task(Base):
     id = Column(Integer, nullable=False, primary_key=True)
     uuid = Column(String, nullable=False)
     name = Column(String, nullable=False)
-    state = Column(String, nullable=False, default="created")
+    state = Column(String, CheckConstraint("state IN ('created', 'running', 'finished')"), nullable=False, default="created")
 
     def __repr__(self) -> str:
         return f"<Task: {self.id}>"
